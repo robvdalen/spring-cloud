e458a1f5dd5393b8400080186c12368a68cbb92a
Rob van Dalen <rob.van.dalen@wolterskluwer.com>
2019-02-08 11:38:39 +0100

    First commit

---
+++ config-service/src/main/java/nl/vdalen/spring/cloud/configservice/ConfigService.java
@@ -0,0 +1,17 @@
+package nl.vdalen.spring.cloud.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@EnableConfigServer
+@RefreshScope
+public class ConfigService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigService.class, args);
+    }
+
+}
