f2f0dcd72383aacb8a53850aee47c20679f86143
Rob van Dalen <rob.van.dalen@wolterskluwer.com>
2019-07-05 14:03:24 +0200

    - Upgraded dependencies - Updated logging - Fixed loading local configs

--- config-service/src/main/java/nl/vdalen/spring/cloud/configservice/ConfigService.java
+++ config-service/src/main/java/nl/vdalen/spring/cloud/configservice/ConfigService.java
@@ -3,11 +3,9 @@ package nl.vdalen.spring.cloud.configservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
-@RefreshScope
 public class ConfigService {
 
     public static void main(String[] args) {
--- discovery-service/src/main/java/nl/vdalen/spring/cloud/discoveryservice/DiscoveryService.java
+++ discovery-service/src/main/java/nl/vdalen/spring/cloud/discoveryservice/DiscoveryService.java
@@ -2,10 +2,12 @@ package nl.vdalen.spring.cloud.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RefreshScope
 public class DiscoveryService {
 	
 	public static void main(String[] args) {
